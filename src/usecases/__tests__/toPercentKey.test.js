cd25598860441440b321d9890515f3780ac3127d
chahyasantoso <chahya.santoso@gmail.com>
2026-07-27 16:23:08 +0700

    test: align percent precision assertion

--- src/usecases/__tests__/toPercentKey.test.js
+++ src/usecases/__tests__/toPercentKey.test.js
@@ -4,7 +4,7 @@ import { toPercentKey } from '../toPercentKey.js';
 describe('toPercentKey', () => {
   it('removes floating-point noise from authored progress', () => {
     expect(toPercentKey(0.29)).toBe('29%');
-    expect(toPercentKey(0.333333333)).toBe('33.3333333%');
+    expect(toPercentKey(0.333333333)).toBe('33.333333%');
   });
 
   it('produces identical keys for equivalent positions', () => {
