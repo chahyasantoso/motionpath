184f1942114451b99e521f676d038f55b06b85bc
chahyasantoso <chahya.santoso@gmail.com>
2026-08-09 11:41:05 +0700

    style(pass2): wrap TrackObservationOwner compose method

--- packages/core/src/usecases/TrackObservationOwner.js
+++ packages/core/src/usecases/TrackObservationOwner.js
@@ -28,17 +28,56 @@ export class TrackObservationOwner {
   get tracks() { return this.#state.tracks; }
   get isDestroyed() { return this.#destroyed; }
 
-  register(track, key = track?.id) { this.#assertAlive(); return this.#state.register(track, key); }
-  unregister(id, options) { if (!this.#destroyed) this.#state.unregister(id, options); }
-  addEdge(edge) { this.#assertAlive(); return this.#state.addEdge(edge); }
-  removeEdge(edge) { if (!this.#destroyed) this.#state.removeEdge(edge); }
-  removeSourceEdges(id) { if (!this.#destroyed) this.#state.removeSourceEdges(id); }
-  replaceEdge(previous, next) { this.#assertAlive(); return this.#state.replaceEdge(previous, next); }
+  register(track, key = track?.id) {
+    this.#assertAlive();
+    return this.#state.register(track, key);
+  }
+
+  unregister(id, options) {
+    if (!this.#destroyed) this.#state.unregister(id, options);
+  }
+
+  addEdge(edge) {
+    this.#assertAlive();
+    return this.#state.addEdge(edge);
+  }
+
+  removeEdge(edge) {
+    if (!this.#destroyed) this.#state.removeEdge(edge);
+  }
+
+  removeSourceEdges(id) {
+    if (!this.#destroyed) this.#state.removeSourceEdges(id);
+  }
+
+  replaceEdge(previous, next) {
+    this.#assertAlive();
+    return this.#state.replaceEdge(previous, next);
+  }
+
   getEdges(id) { return this.#state.getEdges(id); }
   getSources(id) { return this.#state.getSources(id); }
   getObserverIds(id) { return this.#state.getObserverIds(id); }
   getTrack(key) { return this.#state.getTrack(key); }
-  compose(id, rawData, ctx) { this.#assertAlive(); return this.#state.compose(id, rawData, ctx, trackComposeLeaf, this.#composeSource ?? undefined); }
-  destroy() { if (this.#destroyed) return; this.#destroyed = true; this.#state.destroy(); }
-  #assertAlive() { if (this.#destroyed) throw new Error("TrackObservationOwner is destroyed."); }
+
+  compose(id, rawData, ctx) {
+    this.#assertAlive();
+    return this.#state.compose(
+      id,
+      rawData,
+      ctx,
+      trackComposeLeaf,
+      this.#composeSource ?? undefined,
+    );
+  }
+
+  destroy() {
+    if (this.#destroyed) return;
+    this.#destroyed = true;
+    this.#state.destroy();
+  }
+
+  #assertAlive() {
+    if (this.#destroyed) throw new Error("TrackObservationOwner is destroyed.");
+  }
 }
